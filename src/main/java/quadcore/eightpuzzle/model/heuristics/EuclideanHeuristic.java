a2aef95fc437cda0cd9f74ddafc076261487b919
Mariam Ghazi <mariamghazi9@gmail.com>
2021-11-11 20:12:38 +0200

    Removed 0 heuristic

--- src/main/java/quadcore/eightpuzzle/model/heuristics/EuclideanHeuristic.java
+++ src/main/java/quadcore/eightpuzzle/model/heuristics/EuclideanHeuristic.java
@@ -37,13 +37,16 @@ public class EuclideanHeuristic implements ToDoubleFunction<State> {
         Point[] stateCoordinates = getPoints(stateArray);
         Point[] goalCoordinates = getPoints(new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8'});
         double euclidean = 0.0;
+        double [] heuristic = new double[9];
+
         for (int i = 0; i < 9; i++) {
             double diffX = Math.abs(stateCoordinates[i].x - goalCoordinates[i].x);
             double diffY = Math.abs(stateCoordinates[i].y - goalCoordinates[i].y);
-            double distance = Math.sqrt(Math.pow(diffX,2)+Math.pow(diffY,2));
-            euclidean += distance;
+            heuristic[Character.getNumericValue(stateArray[i])]  = Math.sqrt(Math.pow(diffX,2)+Math.pow(diffY,2));
+            euclidean += heuristic[Character.getNumericValue(stateArray[i])] ;
         }
-        return euclidean;
+        return euclidean - heuristic[0];
     }
+
 
 }
--- src/main/java/quadcore/eightpuzzle/model/heuristics/ManhattanHeuristic.java
+++ src/main/java/quadcore/eightpuzzle/model/heuristics/ManhattanHeuristic.java
@@ -38,14 +38,17 @@ public class ManhattanHeuristic implements ToDoubleFunction<State> {
         Point[] stateCoordinates = getPoints(stateArray);
         Point[] goalCoordinates = getPoints(new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8'});
         double manhattan = 0.0;
+        double [] heuristic = new double[9];
         for (int i = 0; i < 9; i++) {
             int diffX = Math.abs(stateCoordinates[i].x - goalCoordinates[i].x);
             int diffY = Math.abs(stateCoordinates[i].y - goalCoordinates[i].y);
-            int distance = diffX + diffY;
-            manhattan += distance;
+            heuristic[Character.getNumericValue(stateArray[i])] = diffX + diffY;
+            manhattan +=  heuristic[Character.getNumericValue(stateArray[i])];
         }
-        return manhattan;
+        return manhattan - heuristic[0];
     }
+
+
 
 }
 
